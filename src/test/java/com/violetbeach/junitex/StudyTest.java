0723b167949208a1812317cddff8625d8d555aad
Jerry(이재헌) <ti6419@gmail.com>
2022-06-01 17:59:31 +0900

    assertThrows 테스트 추가

--- src/test/java/com/violetbeach/junitex/StudyTest.java
+++ src/test/java/com/violetbeach/junitex/StudyTest.java
@@ -10,15 +10,23 @@ public class StudyTest {
     @Test
     @DisplayName("스터디 만들기 😲")
     void create() {
-        Study study = new Study();
+        Study study = new Study(10);
         assertAll(
                 () -> assertNotNull(study),
                 () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
                         () -> "스터디를 처음 만들면 상태값이 DRAFT이어야 한다."),
                 () -> assertTrue(study.getLimit() > 0,
-                        () -> "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
+                        () -> "스터 디 최대 참석 가능 인원은 0보다 커야 한다.")
         );
 
+    }
+
+    @Test
+    @DisplayName("limit은 0보다 커야 한다.")
+    void create_new_study() {
+        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+        assertEquals("limit은 0보다 커야 한다.", exception.getMessage());
     }
 
     @Test
