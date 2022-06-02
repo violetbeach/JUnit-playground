6b5e4a151f1547b73431bf35ba45dff616b8d1d4
Jerry(이재헌) <ti6419@gmail.com>
2022-06-03 08:45:48 +0900

    use mock annotation +MockitoExtension

--- src/test/java/me/whiteship/inflearnthejavatest/study/StudyServiceTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/study/StudyServiceTest.java
@@ -2,18 +2,23 @@ package me.whiteship.inflearnthejavatest.study;
 
 import me.whiteship.inflearnthejavatest.member.MemberService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.mock;
 
+@ExtendWith(MockitoExtension.class)
 public class StudyServiceTest {
 
+    @Mock
+    MemberService memberService;
+
+    @Mock
+    StudyRepository studyRepository;
 
     @Test
     void createStudyService() {
-        MemberService memberService = mock(MemberService.class);
-        StudyRepository studyRepository = mock(StudyRepository.class)
-
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
     }
